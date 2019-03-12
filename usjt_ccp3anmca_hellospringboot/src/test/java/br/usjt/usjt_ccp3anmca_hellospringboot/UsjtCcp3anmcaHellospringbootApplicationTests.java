e7fb9728490ed860b1b0217986462a309cc8d2df
Rodrigo Bossini <professorbossini@gmail.com>
2019-03-12 18:37:56 -0300

    Commit inicial. Projeto apenas criado.

---
+++ usjt_ccp3anmca_hellospringboot/src/test/java/br/usjt/usjt_ccp3anmca_hellospringboot/UsjtCcp3anmcaHellospringbootApplicationTests.java
@@ -0,0 +1,16 @@
+package br.usjt.usjt_ccp3anmca_hellospringboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UsjtCcp3anmcaHellospringbootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
